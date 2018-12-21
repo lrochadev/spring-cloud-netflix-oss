d4322a5f9bac620214e2bb43068ea19b82f3d454
Leonardo Rocha <lrochadev@gmail.com>
2018-12-21 08:02:58 -0200

    Injecting feignclient

--- eureka-client/src/main/java/br/com/lrx/eurekaclient/RestService.java
+++ eureka-client/src/main/java/br/com/lrx/eurekaclient/RestService.java
@@ -1,17 +1,23 @@
 package br.com.lrx.eurekaclient;
 
 import lombok.AllArgsConstructor;
+import lombok.Setter;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+
 @RestController
 @AllArgsConstructor
 @RequestMapping("/api")
 public class RestService {
 	
+	@Autowired
+	@Setter
 	private FeignClient2 feignClient2;  
 
 	@RequestMapping(method = RequestMethod.GET)
